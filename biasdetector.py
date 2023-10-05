f3ac18c852ee6ab24c23be161a683132f24d0070
Jessica Nguyen <89553284+Jnguye84@users.noreply.github.com>
2023-10-05 17:01:01 -0400

    Create biasdetector.py

---
+++ biasdetector.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+from pdf2image import convert_from_path
+import pyperclip as pc
+from os import listdir
+from os.path import isfile, join
+import pytesseract
+from PyPDF2 import PdfWriter, PdfReader
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+import urllib.request
+from numpy import nan
+
+dataset = pd.read_csv('fake.csv') #reading csv
+keywords = ['cryptocurrency','financial','finance','finances', 'money', 'wealth', 'stocks']
+
+#START data cleaning the fake.csv file
+
+dataset = dataset.drop(dataset[dataset['type'] == 'conspiracy'].index) #got rid of conspiracy and bs articles
+dataset = dataset.drop(dataset[dataset['type'] == 'bs'].index)
+new_dataset = pd.DataFrame(columns = ['Text','Title'])
+rowIndex = 0
+
+#for each variable, we need to create a new list in order to add as the new column
+text_col = []
+title_col = []
+author_col = []
+
+for i in dataset['text']:
+  for k in keywords: #if there is at least one k keyword in the article, then add it.
+    if k in str(i):
+      text_col.append(i)
+      rowIndex = dataset.index[dataset['text']== i].tolist() #gets the index of the text column to then get the same title and author
+      title_col.append(dataset['title'][rowIndex])
+      author_col.append(dataset['author'][rowIndex])
+      break #prevents duplicate articles from showing up
+
+new_dataset['Text']=text_col
+new_dataset['Title']= title_col
+new_dataset['author']= author_col
+
+dataset = new_dataset #I've now filtered out all the articles that don't include finance keywords
+
+#END data cleaning fake.csv file
